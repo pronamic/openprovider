2061eae23353eae308e2c0d0056dce01aac66cb5
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-09-05 12:14:59 +0200

    Update monitor-nameservers.sh

--- .github/scripts/monitor-nameservers.sh
+++ .github/scripts/monitor-nameservers.sh
@@ -6,8 +6,15 @@ host -v ns2.openprovider.be > host-ns2.openprovider.be.txt
 
 host -v ns3.openprovider.eu > host-ns3.openprovider.eu.txt
 
+# https://github.com/orgs/community/discussions/26560
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config user.name "github-actions[bot]"
+
 git add --all
 
 git commit --all -m "`date`"
 
 git push origin main
+
+git config --unset user.email
+git config --unset user.name
